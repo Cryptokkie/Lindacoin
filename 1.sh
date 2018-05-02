9bc95ccb842a86352ad1565668d9ac8a2105f998
NerdyUser <jeffrey.de.ceuninck@telenet.be>
2018-05-02 19:47:29 +0200

    Scripts
    Full automated installation scripts

---
+++ 1.sh
@@ -0,0 +1,44 @@
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "NerdyUser: Installation started."
+echo "NerdyUser: This will take some time."
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+sleep 5
+cd ~/
+sudo apt update && sudo apt -y upgrade
+sleep 5
+fallocate -l 3G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+sudo echo -e "/swapfile none swap sw 0 0 \n" >> /etc/fstab
+sleep 5
+sudo apt-get install -y pkg-config
+sleep 5
+sudo apt-get -y install build-essential autoconf automake libtool libboost-all-dev libgmp-dev libssl-dev libcurl4-openssl-dev git
+sleep 5
+yes '' | sudo add-apt-repository ppa:bitcoin/bitcoin
+sleep 5
+sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "NerdyUser: VPS will reboot automatically now!"
+echo "NerdyUser: Reconnect in 2 minutes."
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+sleep 10
+reboot
---
+++ 2.sh
@@ -0,0 +1,71 @@
+cd ~/
+wget https://github.com/Lindacoin/Linda/releases/download/2.0.0.1/Unix.Lindad.v2.0.0.1g.tar.gz
+tar -xzvf Unix.Lindad.v2.0.0.1g.tar.gz -C /usr/local/bin/
+Lindad -daemon
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "NerdyUser: Masternode started for 30 seconds, will be automatic stopped & script will continue!"
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+sleep 30
+Lindad stop
+cd ~/
+clear
+echo -n "Enter your private key & press Enter: "
+read masternodegenkey
+echo "rpcuser=asupersecureusername
+rpcpassword=anevenmoresecurepassword
+rpcallowip=127.0.0.1
+server=1
+listen=1
+daemon=1
+logtimestamps=1
+maxconnections=256
+addnode=seed1.linda-wallet.com
+addnode=seed2.linda-wallet.com
+addnode=seed3.linda-wallet.com
+addnode=seed4.linda-wallet.com
+addnode=seed5.linda-wallet.com
+addnode=seed6.linda-wallet.com
+addnode=seed7.linda-wallet.com
+addnode=seed8.linda-wallet.com
+addnode=seed9.linda-wallet.com
+addnode=seed10.linda-wallet.com
+addnode=seed11.linda-wallet.com
+masternode=1
+masternodeprivkey=$masternodegenkey" > ~/.Linda/Linda.conf
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "NerdyUser: Config saved"
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+cd ~/
+rm -rf Lindacoin
+rm -rf Unix.Lindad.v2.0.0.1g.tar.gz
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "NerdyUser: Installation completed!"
+echo "NerdyUser: You can now bootstrap your VPS by FileZilla!"
+echo " "
+echo "NerdyUser: Perform by yourself: cd /root/"
+echo "NerdyUser: before you continue"
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
