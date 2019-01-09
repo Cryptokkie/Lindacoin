0873171a69d200895bb7dad75b989a4be6a5fcc6
NerdyUser <mrnerdyuser@gmail.com>
2019-01-09 09:04:24 +0100

    v3.2.1.0

--- upgrade32000to3210.sh
+++ upgrade32000to3210.sh
@@ -6,6 +6,7 @@ echo " "
 echo " "
 echo " "
 echo "Upgrade started."
+echo "Cancel this action if you didn't had v3.2.0.0 running!"
 echo " "
 echo " "
 echo " "
@@ -16,17 +17,6 @@ echo " "
 echo "Current wallet stopped, wait 10 seconds."
 echo " "
 sleep 10
-rm -rf .Linda/database
-rm -rf .Linda/txleveldb
-rm .Linda/blk0001.dat
-rm .Linda/db.log
-rm .Linda/debug.log
-rm .Linda/peers.dat
-echo " "
-echo "Deleted unneeded files & folders."
-echo "Starting new installation, wait 5 seconds."
-echo " "
-sleep 5
 wget https://github.com/TheLindaProjectInc/Linda/releases/download/3.2.1.0/Lindad-linux-x64.tar.gz
 tar -xzvf Lindad-linux-x64.tar.gz -C /usr/local/bin/
 rm /usr/local/bin/Linda-cli
@@ -36,28 +26,6 @@ echo " "
 echo "Wallet started, wait 30 seconds."
 echo " "
 sleep 30
-Lindad stop
-echo " "
-echo "Wallet stopped, wait 5 seconds."
-echo " "
-sleep 5
-echo " "
-echo " "
-echo " "
-echo " "
-echo " "
-Lindad
-echo " "
-echo "Wallet started, wait 30 seconds."
-echo " "
-sleep 30
-echo " "
-echo " "
-echo " "
-echo " "
-echo " "
-echo "Check below if you have connections"
-echo "and more than 0 blocks."
 echo " "
 echo " "
 echo " "
@@ -67,6 +35,9 @@ Lindad getinfo
 echo " "
 echo " "
 echo " "
+echo " "
+echo "Masternode Status:"
+Lindad masternode start
 echo " "
 echo " "
 echo " "
